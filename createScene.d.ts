25b197fed7c78cf157059caf0145e09622af06e8
Max <makayun@gmail.com>
2025-04-03 16:30:22 +0200

    Updates

---
+++ createScene.d.ts
@@ -0,0 +1,3 @@
+import * as BABYLON from "babylonjs";
+export declare function createScene(engine: BABYLON.Engine, canvas: HTMLElement): BABYLON.Scene;
+//# sourceMappingURL=createScene.d.ts.map
---
+++ gui.d.ts
@@ -0,0 +1,8 @@
+import { AbstractMesh } from "babylonjs";
+import * as GUI from "babylonjs-gui";
+export declare function createSimpleButton(text: string): GUI.Button;
+export declare function createImageButton(text: string, imageUrl: string): GUI.Button;
+export declare function createTextBlock(in_text: string, in_color: string, in_fontSize: number): GUI.TextBlock;
+export declare function createInputText(in_text?: string): GUI.InputText;
+export declare function addLabelToMesh(mesh: AbstractMesh): void;
+//# sourceMappingURL=gui.d.ts.map
