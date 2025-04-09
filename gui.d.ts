2af20de721b8dc553eba0979da9becfefa6fe027
Maksim Makagonov <maxmakagonov@Maksims-MacBook-Pro.local>
2025-04-09 17:57:10 +0200

    Updates

---
+++ gui.d.ts
@@ -0,0 +1,10 @@
+import { AbstractMesh } from "babylonjs";
+import * as GUI from "babylonjs-gui";
+export declare function createSimpleButton(text: string): GUI.Button;
+export declare function createImageButton(text: string, imageUrl: string): GUI.Button;
+export declare function createTextBlock(in_text: string, in_color: string, in_fontSize: number): GUI.TextBlock;
+export declare function createInputText(in_text?: string): GUI.InputText;
+export declare function createSlider(in_min: number, in_max: number): GUI.Slider;
+export declare function createPicker(): GUI.ColorPicker;
+export declare function addLabelToMesh(mesh: AbstractMesh): void;
+//# sourceMappingURL=gui.d.ts.map
