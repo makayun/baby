6aeb5e10c006d2fc890b2ad968e8dd35d4513ff5
Max <makayun@gmail.com>
2025-04-10 13:39:53 +0200

    time to do the pong

--- src/createScene.ts
+++ src/createScene.ts
@@ -1,82 +1,41 @@
-import * as GUI from "./gui";
+// import * as GUI from "./gui";
 import * as BABYLON from "babylonjs"
-// import { InputText } from "babylonjs-gui";
-// import plyBtn from "./assets/play-button.svg"
-
+// import { Control } from "babylonjs-gui";
 import { ddsAssets } from "./import_assets";
+// import { svgAssets } from "./import_assets";
 
-export function createScene(engine: BABYLON.Engine, canvas: HTMLElement): BABYLON.Scene {
-    let scene: BABYLON.Scene = new BABYLON.Scene(engine);
+function createBall(scene: BABYLON.Scene, metal: BABYLON.PBRMaterial) : BABYLON.Mesh {
+    let ball = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+    ball.material = metal;
+    return ball;
+}
 
-    let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 2, -15), scene);
-    camera.attachControl(canvas, true);
-
-    let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-	let sphere2 = sphere.clone();
-	let sphere3 = sphere.clone();
-
-    // const hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData(sky1, scene);
-    const hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData(ddsAssets['sky1'], scene);
-    const skybox : BABYLON.AbstractMesh | any = scene.createDefaultSkybox(hdrTexture, true, 10000);
-
-    let probe = new BABYLON.ReflectionProbe("main", 512, scene);
-    if (probe.renderList)
-        probe.renderList.push(sphere2, sphere3, skybox);
-
+function createMetal(scene: BABYLON.Scene, probe: BABYLON.ReflectionProbe) : BABYLON.PBRMaterial {
     let metal = new BABYLON.PBRMaterial('metal', scene);
     metal.reflectionTexture = probe.cubeTexture;
     metal.roughness = 0.25;
     metal.metallic = 1.0;
     metal.realTimeFiltering = true;
     metal.realTimeFilteringQuality = BABYLON.Constants.TEXTURE_FILTERING_QUALITY_HIGH;
-    sphere.material = metal;
+}
 
-    sphere2.setPivotMatrix(BABYLON.Matrix.Translation(3, 3, 0), false);
-    let metal2 = new BABYLON.PBRMaterial('metal', scene);
-    metal2.albedoColor = BABYLON.Color3.Magenta();
-    metal2.roughness = 0.1;
-    metal2.metallic = 0.0;
-    sphere2.material = metal2;
+export function createScene(engine: BABYLON.Engine, canvas: HTMLElement): BABYLON.Scene {
+    let scene: BABYLON.Scene = new BABYLON.Scene(engine);
 
-    sphere3.setPivotMatrix(BABYLON.Matrix.Translation(6, 1, 0), false);
-    // sphere3.material = new BABYLON.PBRMaterial('metal', scene);
-    // (sphere3.material as BABYLON.PBRMaterial).roughness = 0.1;
-    // (sphere3.material as BABYLON.PBRMaterial).metallic = 0.3;
-    // (sphere3.material as BABYLON.PBRMaterial).albedoColor = new BABYLON.Color3(0.3, 0.0, 0.8);
-    let metal3 = new BABYLON.PBRMaterial('metal', scene);
-    metal3.roughness = 0.1;
-    metal3.metallic = 0.3;
-    metal3.albedoColor = new BABYLON.Color3(0.3, 0.0, 0.8);
-    sphere3.material = metal3;
+    let camera = new BABYLON.ArcRotateCamera("camera", 1, 1, 15, BABYLON.Vector3.Zero(), scene);
+    camera.lowerRadiusLimit = 5;
+    camera.attachControl(canvas, true);
 
-    scene.registerBeforeRender(function () {
-        sphere2.rotation.y += 0.01;
-        sphere3.rotation.y += 0.01;
-    })
+    const hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData(ddsAssets['sky1'], scene);
+    const skybox : BABYLON.AbstractMesh | any = scene.createDefaultSkybox(hdrTexture, true, 10000);
 
+    let probe = new BABYLON.ReflectionProbe("main", 512, scene);
+    if (probe.renderList)
+        probe.renderList.push(skybox);
 
-    // GUI.addLabelToMesh(sphere);
-    const random_color_btn = GUI.createSimpleButton("Click me!");
-    random_color_btn.onPointerClickObservable.add(function() {
-        metal3.albedoColor = BABYLON.Color3.Random();
-        metal2.albedoColor = BABYLON.Color3.Random();
-    })
-    // GUI.createImageButton("Play", "./assets/play-button.svg")
-    // GUI.createImageButton("Play", plyBtn);
-    // GUI.createTextBlock('Hello, Babylon!', 'white', 40);
-    // const inputTextBlock = GUI.createInputText('0.1');
-    // inputTextBlock.onEnterPressedObservable.add(function (value) {
-    //     if (value instanceof InputText)
-    //         console.log(value.text);
-    // })
-    // const slider = GUI.createSlider(0, 3);
-    // slider.onValueChangedObservable.add(function (value) {
-    //     sphere.position.z = value;
-    // })
-    // const picker = GUI.createPicker();
-    // picker.onValueChangedObservable.add(function (value) {
-    //     metal2.albedoColor.copyFrom(value);
-    // })
+    const metal = createMetal(scene, probe);
+    const ball = createBall(scene, metal);
+    camera.parent = ball;
 
     scene.executeWhenReady(() => {
         engine.hideLoadingUI();
--- src/gui.ts
+++ src/gui.ts
@@ -3,28 +3,36 @@ import * as GUI from "babylonjs-gui"
 
 let advancedTexture: GUI.AdvancedDynamicTexture;
 
-// This must be added in the beginning of every function that creates a GUI element!!!
-function guiInit(): void {
+function addControl(guiElement: GUI.Control): void {
     if (!advancedTexture) {
         advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("ui1");
     }
+
+    advancedTexture.addControl(guiElement);
 }
 
-export function createSimpleButton(text: string) : GUI.Button {
-    guiInit();
-
+export function createSimpleButton(
+        text: string,
+        onClick?: () => void) : GUI.Button {
     const button = GUI.Button.CreateSimpleButton(`${text}_btn`, text);
     button.width = 0.2;
     button.height = '40px';
     button.color = 'white';
     button.background = 'deepskyblue';
-    advancedTexture.addControl(button);
+
+    addControl(button);
+
+    if (onClick) {
+        button.onPointerClickObservable.add(onClick);
+    }
+
     return (button);
 }
 
-export function createImageButton(text: string, imageUrl: string) {
-    guiInit();
-
+export function createImageButton(
+        text: string,
+        imageUrl: string,
+        onClick?: () => void) : GUI.Button {
     const button = GUI.Button.CreateImageButton(`${text}_imgbtn`, text, imageUrl);
     button.width = 0.2;
     button.height = 0.04;
@@ -34,13 +42,17 @@ export function createImageButton(text: string, imageUrl: string) {
         button.image.left = '5%';
     if (button.textBlock != undefined)
         button.textBlock.fontSize = '50%';
-    advancedTexture.addControl(button);
+
+    addControl(button);
+
+    if (onClick) {
+        button.onPointerClickObservable.add(onClick);
+    }
+
     return (button);
 }
 
 export function createTextBlock(in_text: string, in_color: string, in_fontSize: number) : GUI.TextBlock {
-    guiInit();
-
     const text_block = new GUI.TextBlock();
     text_block.text = in_text;
     text_block.color = in_color;
@@ -49,14 +61,20 @@ export function createTextBlock(in_text: string, in_color: string, in_fontSize: 
     text_block.shadowColor = '#000';
     text_block.shadowOffsetX = 2;
     text_block.shadowOffsetY = 2;
-    advancedTexture.addControl(text_block);
+
+    addControl(text_block);
 
     return (text_block);
 }
 
-export function createInputText(in_text?: string) : GUI.InputText {
-    guiInit();
-
+export function createInputText(
+        in_text?: string,
+        events?: {
+            onEnterPressed?: (value: string) => void,
+            onTextChanged?: (value: string) => void;
+            onFocus?: () => void;
+            onBlur?: () => void;
+        }) : GUI.InputText {
     const inputTextBlock = new GUI.InputText(`${in_text}_inputtxt`, in_text);
     inputTextBlock.width = 0.2;
     inputTextBlock.height = '40px';
@@ -64,41 +82,64 @@ export function createInputText(in_text?: string) : GUI.InputText {
     inputTextBlock.background = 'deepskyblue';
     inputTextBlock.focusedBackground = 'white';
 
-    advancedTexture.addControl(inputTextBlock);
+    if (events?.onEnterPressed) {
+        inputTextBlock.onEnterPressedObservable.add((input) => {
+            if (input instanceof GUI.InputText)
+                events.onEnterPressed?.(input.text);
+        });
+    }
 
+    if (events?.onTextChanged) {
+        inputTextBlock.onTextChangedObservable.add((evt) => {
+            events.onTextChanged?.(evt.text);
+        });
+    }
+    if (events?.onFocus) {
+        inputTextBlock.onFocusObservable.add(events.onFocus);
+    }
+    if (events?.onBlur) {
+        inputTextBlock.onBlurObservable.add(events.onBlur);
+    }
+
+    addControl(inputTextBlock);
     return inputTextBlock;
 }
 
-export function createSlider(in_min: number, in_max: number) : GUI.Slider {
-    guiInit();
 
+export function createSlider(in_min: number, in_max: number) : GUI.Slider {
     let slider = new GUI.Slider;
     slider.minimum = in_min;
     slider.maximum = in_max;
     slider.width = 0.2;
     slider.height = '20px';
     slider.value = in_min;
-    advancedTexture.addControl(slider);
+
+    addControl(slider);
 
     return slider;
 }
 
-export function createPicker() {
-    guiInit();
-
+export function createPicker() : GUI.ColorPicker {
     const picker = new GUI.ColorPicker;
-    advancedTexture.addControl(picker);
+
+    addControl(picker);
+
     return picker;
 }
 
-// export function createCheckbox() {
+export function createCheckbox() : GUI.Checkbox {
+    const checkbox = new GUI.Checkbox();
+    checkbox.width = checkbox.height = '20px';
+    checkbox.color = 'white';
+    checkbox.isChecked = false;
 
-// }
+    addControl(checkbox);
+
+    return checkbox;
+}
 
 
-export function addLabelToMesh(mesh: AbstractMesh): void {
-    guiInit();
-
+export function addLabelToMesh(mesh: AbstractMesh): GUI.Rectangle {
     let label: GUI.Rectangle = new GUI.Rectangle("label for " + mesh.name);
     label.background = "black";
     label.height = "30px";
@@ -110,11 +151,13 @@ export function addLabelToMesh(mesh: AbstractMesh): void {
     label.top = "10%";
     label.zIndex = 5;
     label.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
-    advancedTexture.addControl(label);
+    addControl(label);
 
     const text1: GUI.TextBlock = new GUI.TextBlock();
     text1.text = mesh.name;
     text1.color = "white";
     label.addControl(text1);
+
+    return (label);
 }
 
