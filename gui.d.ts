09e765388aeb77ba6e228d75c39f51884f5cb20c
Max <makayun@gmail.com>
2025-04-10 11:45:37 +0200

    Updates

--- gui.d.ts
+++ gui.d.ts
@@ -1,10 +1,16 @@
 import { AbstractMesh } from "babylonjs";
 import * as GUI from "babylonjs-gui";
-export declare function createSimpleButton(text: string): GUI.Button;
-export declare function createImageButton(text: string, imageUrl: string): GUI.Button;
+export declare function createSimpleButton(text: string, onClick?: () => void): GUI.Button;
+export declare function createImageButton(text: string, imageUrl: string, onClick?: () => void): GUI.Button;
 export declare function createTextBlock(in_text: string, in_color: string, in_fontSize: number): GUI.TextBlock;
-export declare function createInputText(in_text?: string): GUI.InputText;
+export declare function createInputText(in_text?: string, events?: {
+    onEnterPressed?: (value: string) => void;
+    onTextChanged?: (value: string) => void;
+    onFocus?: () => void;
+    onBlur?: () => void;
+}): GUI.InputText;
 export declare function createSlider(in_min: number, in_max: number): GUI.Slider;
 export declare function createPicker(): GUI.ColorPicker;
-export declare function addLabelToMesh(mesh: AbstractMesh): void;
+export declare function createCheckbox(): GUI.Checkbox;
+export declare function addLabelToMesh(mesh: AbstractMesh): GUI.Rectangle;
 //# sourceMappingURL=gui.d.ts.map
