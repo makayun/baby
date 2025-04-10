4a6f365d45f3776a732b3bc67f8c9cb53b1f1a40
Max <makayun@gmail.com>
2025-04-10 15:23:52 +0200

    stupid physics wont work

--- src/createScene.ts
+++ src/createScene.ts
@@ -4,27 +4,31 @@ import * as BABYLON from "babylonjs"
 import { ddsAssets } from "./import_assets";
 // import { svgAssets } from "./import_assets";
 
-function createBall(scene: BABYLON.Scene, metal: BABYLON.PBRMaterial) : BABYLON.Mesh {
+function createBall(scene: BABYLON.Scene, metal?: BABYLON.PBRMaterial) : BABYLON.Mesh {
     let ball = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+    if (metal)
     ball.material = metal;
     return ball;
 }
 
-function createMetal(scene: BABYLON.Scene, probe: BABYLON.ReflectionProbe) : BABYLON.PBRMaterial {
+function createMetal(scene: BABYLON.Scene, probe?: BABYLON.ReflectionProbe) : BABYLON.PBRMaterial {
     let metal = new BABYLON.PBRMaterial('metal', scene);
-    metal.reflectionTexture = probe.cubeTexture;
+    if (probe)
+        metal.reflectionTexture = probe.cubeTexture;
     metal.roughness = 0.25;
     metal.metallic = 1.0;
     metal.realTimeFiltering = true;
     metal.realTimeFilteringQuality = BABYLON.Constants.TEXTURE_FILTERING_QUALITY_HIGH;
+    return metal;
 }
 
 export function createScene(engine: BABYLON.Engine, canvas: HTMLElement): BABYLON.Scene {
     let scene: BABYLON.Scene = new BABYLON.Scene(engine);
 
-    let camera = new BABYLON.ArcRotateCamera("camera", 1, 1, 15, BABYLON.Vector3.Zero(), scene);
+    let camera = new BABYLON.ArcRotateCamera("camera", 1, 1, 30, BABYLON.Vector3.Zero(), scene);
     camera.lowerRadiusLimit = 5;
     camera.attachControl(canvas, true);
+
 
     const hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData(ddsAssets['sky1'], scene);
     const skybox : BABYLON.AbstractMesh | any = scene.createDefaultSkybox(hdrTexture, true, 10000);
@@ -36,6 +40,16 @@ export function createScene(engine: BABYLON.Engine, canvas: HTMLElement): BABYLO
     const metal = createMetal(scene, probe);
     const ball = createBall(scene, metal);
     camera.parent = ball;
+    const beh = new BABYLON.BouncingBehavior;
+    ball.addBehavior(beh);
+    ball.position.y = 4;
+
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 10, height: 10}, scene);
+    ground.intersectsMesh(ball);
+
+    var gravityVector = new BABYLON.Vector3(0, -9.81, 0);
+    var physicsPlugin = new BABYLON.HavokPlugin();
+    scene.enablePhysics(gravityVector, physicsPlugin);
 
     scene.executeWhenReady(() => {
         engine.hideLoadingUI();
