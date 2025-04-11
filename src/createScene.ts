cec6fdd2fe53604512a8cc7500069178ff1cd5cd
Maksim Makagonov <maxmakagonov@Maksims-MacBook-Pro.local>
2025-04-11 14:52:38 +0200

    lala

--- src/createScene.ts
+++ src/createScene.ts
@@ -1,75 +1,85 @@
-// import * as GUI from "./gui";
+import * as GUI from "./gui";
 import * as BABYLON from "babylonjs"
-// import { Control } from "babylonjs-gui";
-import { ddsAssets, pngAssets, svgAssets } from "./import_assets";
+// import { InputText } from "babylonjs-gui";
+// import plyBtn from "./assets/play-button.svg"
 
-function createBall(scene: BABYLON.Scene, metal?: BABYLON.PBRMaterial) : BABYLON.Mesh {
-    let ball = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-    if (metal)
-    ball.material = metal;
-    return ball;
-}
-
-function createMetal(scene: BABYLON.Scene, probe?: BABYLON.ReflectionProbe) : BABYLON.PBRMaterial {
-    let metal = new BABYLON.PBRMaterial('metal', scene);
-    if (probe)
-        metal.reflectionTexture = probe.cubeTexture;
-    metal.roughness = 0.25;
-    metal.metallic = 1.0;
-    metal.realTimeFiltering = true;
-    metal.realTimeFilteringQuality = BABYLON.Constants.TEXTURE_FILTERING_QUALITY_HIGH;
-    return metal;
-}
-
+import { ddsAssets } from "./import_assets";
 
 export function createScene(engine: BABYLON.Engine, canvas: HTMLElement): BABYLON.Scene {
     let scene: BABYLON.Scene = new BABYLON.Scene(engine);
-    var gravityVector = new BABYLON.Vector3(0,-9.81, 0);
-    var physicsPlugin = new BABYLON.CannonJSPlugin();
-    scene.enablePhysics(gravityVector, physicsPlugin);
 
-
-    const light = new BABYLON.HemisphericLight("light", BABYLON.Vector3.Zero(), scene);
-
-    let camera = new BABYLON.ArcRotateCamera("camera", 1, 1, 30, BABYLON.Vector3.Zero(), scene);
-    camera.lowerRadiusLimit = 5;
+    let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 2, -15), scene);
     camera.attachControl(canvas, true);
 
+    let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+	let sphere2 = sphere.clone();
+	let sphere3 = sphere.clone();
 
+    // const hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData(sky1, scene);
     const hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData(ddsAssets['sky1'], scene);
     const skybox : BABYLON.AbstractMesh | any = scene.createDefaultSkybox(hdrTexture, true, 10000);
 
     let probe = new BABYLON.ReflectionProbe("main", 512, scene);
     if (probe.renderList)
-        probe.renderList.push(skybox);
+        probe.renderList.push(sphere2, sphere3, skybox);
 
-    const metal = createMetal(scene, probe);
-    const ball = createBall(scene, metal);
-    camera.parent = ball;
-    light.parent = ball;
-    const beh = new BABYLON.BouncingBehavior;
-    ball.addBehavior(beh);
-    ball.position.y = 5;
+    let metal = new BABYLON.PBRMaterial('metal', scene);
+    metal.reflectionTexture = probe.cubeTexture;
+    metal.roughness = 0.25;
+    metal.metallic = 1.0;
+    metal.realTimeFiltering = true;
+    metal.realTimeFilteringQuality = BABYLON.Constants.TEXTURE_FILTERING_QUALITY_HIGH;
+    sphere.material = metal;
 
-    var groundSize = 50;
-    var groundMaxHeight = 2;
+    sphere2.setPivotMatrix(BABYLON.Matrix.Translation(3, 3, 0), false);
+    let metal2 = new BABYLON.PBRMaterial('metal', scene);
+    metal2.albedoColor = BABYLON.Color3.Magenta();
+    metal2.roughness = 0.1;
+    metal2.metallic = 0.0;
+    sphere2.material = metal2;
 
-    // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("ground", pngAssets["height_map"], {
-        width: groundSize,
-        height: groundSize,
-        maxHeight: groundMaxHeight,
-        subdivisions: 100
+    sphere3.setPivotMatrix(BABYLON.Matrix.Translation(6, 1, 0), false);
+    // sphere3.material = new BABYLON.PBRMaterial('metal', scene);
+    // (sphere3.material as BABYLON.PBRMaterial).roughness = 0.1;
+    // (sphere3.material as BABYLON.PBRMaterial).metallic = 0.3;
+    // (sphere3.material as BABYLON.PBRMaterial).albedoColor = new BABYLON.Color3(0.3, 0.0, 0.8);
+    let metal3 = new BABYLON.PBRMaterial('metal', scene);
+    metal3.roughness = 0.1;
+    metal3.metallic = 0.3;
+    metal3.albedoColor = new BABYLON.Color3(0.3, 0.0, 0.8);
+    sphere3.material = metal3;
+
+    scene.registerBeforeRender(function () {
+        sphere2.rotation.y += 0.01;
+        sphere3.rotation.y += 0.01;
     })
 
-    let gag: BABYLON.PhysicsAggregate;
-    let sag: BABYLON.PhysicsAggregate;
+
+    // GUI.addLabelToMesh(sphere);
+    const random_color_btn = GUI.createSimpleButton("Click me!");
+    random_color_btn.onPointerClickObservable.add(function() {
+        metal3.albedoColor = BABYLON.Color3.Random();
+        metal2.albedoColor = BABYLON.Color3.Random();
+    })
+    // GUI.createImageButton("Play", "./assets/play-button.svg")
+    // GUI.createImageButton("Play", plyBtn);
+    // GUI.createTextBlock('Hello, Babylon!', 'white', 40);
+    // const inputTextBlock = GUI.createInputText('0.1');
+    // inputTextBlock.onEnterPressedObservable.add(function (value) {
+    //     if (value instanceof InputText)
+    //         console.log(value.text);
+    // })
+    // const slider = GUI.createSlider(0, 3);
+    // slider.onValueChangedObservable.add(function (value) {
+    //     sphere.position.z = value;
+    // })
+    // const picker = GUI.createPicker();
+    // picker.onValueChangedObservable.add(function (value) {
+    //     metal2.albedoColor.copyFrom(value);
+    // })
 
     scene.executeWhenReady(() => {
         engine.hideLoadingUI();
-        gag = new BABYLON.PhysicsAggregate(ground, BABYLON.PhysicsShapeType.MESH, {mass: 0}, scene);
-        sag = new BABYLON.PhysicsAggregate(ball, BABYLON.PhysicsShapeType.BOX, {mass: 1}, scene);
-
     })
 
     return scene;
