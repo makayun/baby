3a69ca4a4218be58be9999de5c1a6ea651ff2c84
Max <makayun@gmail.com>
2025-04-11 16:09:42 +0200

    it will be a fight

--- src/externals/havok.ts
+++ src/externals/havok.ts
@@ -1,3 +1,8 @@
 import HavokPlugin from "@babylonjs/havok";
 
 export const havokModule = HavokPlugin();
+
+// or:
+// import { HavokPlugin } from "@babylonjs/core";
+
+// export const havokModule =  new HavokPlugin();
